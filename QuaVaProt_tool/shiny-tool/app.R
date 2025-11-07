0daf233d464e42bbf76ef02644fa9e04b8a9bcaf
costa-blidjios <144848142+costa-blidjios@users.noreply.github.com>
2025-11-07 01:05:33 -0500

    Add files via upload

--- QuaVaProt_tool/shiny-tool/app.R
+++ QuaVaProt_tool/shiny-tool/app.R
@@ -360,7 +360,6 @@ server <- function(input, output, session) {
     if(bg_process$values == TRUE){
       if(bg_results$values$is_alive()){
         progress = bg_results$values$read_output_lines()
-        tmp <<- progress
         if(length(progress) != 0){
           progress_check = which(grepl("Progress", progress, fixed = T))
           if(!is.null(progress_check) && length(progress_check) > 0){
